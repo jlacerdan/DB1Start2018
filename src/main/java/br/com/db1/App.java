af201c1cea778481ac9e5e5032ec42433a5442dc
João Lacerda Nascimento <joao.lacerda0@gmail.com>
2018-08-15 09:43:20 -0300

    moar stuff

--- src/main/java/br/com/db1/App.java
+++ src/main/java/br/com/db1/App.java
@@ -1,13 +1,54 @@
 package br.com.db1;
 
+import java.math.BigDecimal;
+
 /**
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
+public class App {
+	public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+        
+        
+        //int zero = 0;
+        Integer zeroObjeto = 0;
+        
+        char vogal = a;
+        String vogalString = "a";
+        
+        double umPontoDois = 1.2;
+        Double valor = 2.0;
+        
+        Float valor3 = 3f;
+        
+        BigDecimal dinheiro = BigDecimal.ZERO;
+        
+        for (Integer x = 0; x < 100; x++){
+        	
+        }
+        
+        Integer x = 0;
+        while (x < 100){
+        	
+        	x++;
+        }
+        
+        do{
+        	
+        } while (x<100);
+
+        
+        x % 2 == 0 : "eh par" ? "eh impar";
+        x % 2 == 0 : "eh par" ? x % 3 == 0 : "wtf" ? "q";
+        if(){
+        	
+        }else if(){
+        	
+        }else{
+        	
+        }
+        
     }
 }
---
+++ src/main/java/br/com/db1/type/EstadosDoSulDoBrasil.java
@@ -0,0 +1,6 @@
+package br.com.db1.type;
+
+public enum EstadosDoSulDoBrasil {
+	
+	PARANA, SANTA_CATARINA, RIO_GRANDE_DO_SUL;
+}
