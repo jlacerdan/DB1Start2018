42c67bd7f03700afe470bd5f29888649a8743a9d
João Lacerda <joao.lacerad0@gmail.com>
2018-08-18 09:03:03 -0300

    18/08/2018 IT WORKS

--- src/main/java/br/com/db1/type/EstadosDoBrasil.java
+++ src/main/java/br/com/db1/type/EstadosDoBrasil.java
@@ -1,69 +1,98 @@
 package br.com.db1.type;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public enum EstadosDoBrasil {
-	ACRE("Norte"), ALAGOAS("Nordeste"), AMAPA("Norte"), AMAZONAS("Norte"), BAHIA("Nordeste"), CEARA("Nordeste"), 
-	DISTRITO_FEDERAL("Centro Oeste"),ESPIRITO_SANTO("Sudeste"), GOIAS("Centro Oeste"), MARANHAO("Nordeste"), MATO_GROSSO("Centro Oeste"),
-	MATO_GROSSO_DO_SUL("Centro Oeste"),	MINAS_GERAIS("Sudeste"), PARA("Norte"), PARAIBA("Nordeste"), PARANA("Sul"), PERNAMBUCO("Nordeste"),
-	PIAUI("Nordeste"), RIO_DE_JANEIRO("Sudeste"), RIO_GRANDE_DO_NORTE("Nordeste"), RIO_GRANDE_DO_SUL("Sul"), RONDONIA("Norte"),
-	RORAIMA("Norte"),SANTA_CATARINA("Sul"), SAO_PAULO("Sudeste"), SERGIPE("Nordeste"), TOCANTINS("Norte");
-	
+	ACRE("Norte", "Acre"), ALAGOAS("Nordeste", "Alagoas"), AMAPA("Norte",
+			"Amapá"), AMAZONAS("Norte", "Amazonas"), BAHIA("Nordeste", "Bahia"), CEARA(
+			"Nordeste", "Ceará"), DISTRITO_FEDERAL("Centro Oeste",
+			"Distrito Federal"), ESPIRITO_SANTO("Sudeste", "Espírito Santo"), GOIAS(
+			"Centro Oeste", "Goiás"), MARANHAO("Nordeste", "Maranhão"), MATO_GROSSO(
+			"Centro Oeste", "Mato Grosso"), MATO_GROSSO_DO_SUL("Centro Oeste",
+			"Mato Grosso do Sul"), MINAS_GERAIS("Sudeste", "Minas Gerais"), PARA(
+			"Norte", "Pará"), PARAIBA("Nordeste", "Paraíba"), PARANA("Sul",
+			"Paraná"), PERNAMBUCO("Nordeste", "Pernambuco"), PIAUI("Nordeste",
+			"Piauí"), RIO_DE_JANEIRO("Sudeste", "Rio De Janeiro"), RIO_GRANDE_DO_NORTE(
+			"Nordeste", "Rio Grande do Norte"), RIO_GRANDE_DO_SUL("Sul",
+			"Rio Grande do Sul"), RONDONIA("Norte", "Rondônia"), RORAIMA(
+			"Norte", "Roraima"), SANTA_CATARINA("Sul", "Santa Catarina"), SAO_PAULO(
+			"Sudeste", "São Paulo"), SERGIPE("Nordeste", "Sergipe"), TOCANTINS(
+			"Norte", "Tocantins");
+
 	private String regiao;
-	
-	EstadosDoBrasil(String regiao){
+	private String nomeEstado;
+
+	EstadosDoBrasil(String regiao, String nomeEstado) {
 		this.regiao = regiao;
+		this.nomeEstado = nomeEstado;
 	}
-	
-	public String regiao(){
+
+	public String regiao() {
 		return regiao;
 	}
-	
-	EstadosDoBrasil[] estados = EstadosDoBrasil.values();
-	
-	public void estadosRegiaoSul(){		
-		
-		for (EstadosDoBrasil estado : estados){
-			if(estado.regiao() == "Sul"){
-				System.out.println(estado + " é da região sul");
-			}
-		}
-		System.out.println();
-	}
-		
-	public void estadosRegiaoSudeste(){
-		for (EstadosDoBrasil estado : estados){
-			if(estado.regiao() == "Sudeste"){
-				System.out.println(estado + " é da região sudeste");
-			}
-		}
-		System.out.println();
-		
-	}
-		
-		public void estadosRegiaoCentroOeste(){
-			for (EstadosDoBrasil estado : estados){
-				if(estado.regiao() == "Centro Oeste"){
-					System.out.println(estado + " é da região Centro Oeste");
-				}
-			}
-			System.out.println();
-		}
-		
-		public void estadosRegiaoNorte(){
-			for (EstadosDoBrasil estado : estados){
-				if(estado.regiao() == "Norte"){
-					System.out.println(estado + " é da região Norte");
-				}
-			}
-			System.out.println();
-		}
-		public void estadosRegiaoNordeste(){
-			for (EstadosDoBrasil estado : estados){
-				if(estado.regiao() == "Nordeste"){
-					System.out.println(estado + " é da região Nordeste");
 
-					}
-		}
+	public String nomeEstado() {
+		return nomeEstado;
 	}
-	
-	
+
+	static EstadosDoBrasil[] estados = EstadosDoBrasil.values();
+
+	public static List<String> estadosRegiaoSul() {
+
+		List<String> lista = new ArrayList<String>();
+		for (EstadosDoBrasil estado : estados) {
+			if (estado.regiao() == "Sul") {
+				lista.add(estado.nomeEstado);
+			}
+		}
+		System.out.println();
+		return lista;
+	}
+
+	public static List<String> estadosRegiaoSudeste() {
+		List<String> lista = new ArrayList<String>();
+		for (EstadosDoBrasil estado : estados) {
+			if (estado.regiao() == "Sudeste") {
+				lista.add(estado.nomeEstado);
+			}
+		}
+		System.out.println();
+		return lista;
+
+	}
+
+	public static List<String> estadosRegiaoCentroOeste() {
+		List<String> lista = new ArrayList<String>();
+		for (EstadosDoBrasil estado : estados) {
+			if (estado.regiao() == "Centro Oeste") {
+				lista.add(estado.nomeEstado);
+			}
+		}
+		System.out.println();
+		return lista;
+	}
+
+	public static List<String> estadosRegiaoNorte() {
+		List<String> lista = new ArrayList<String>();
+		for (EstadosDoBrasil estado : estados) {
+			if (estado.regiao() == "Norte") {
+				lista.add(estado.nomeEstado);
+			}
+		}
+		System.out.println();
+		return lista;
+	}
+
+	public static List<String> estadosRegiaoNordeste() {
+		List<String> lista = new ArrayList<String>();
+		for (EstadosDoBrasil estado : estados) {
+			if (estado.regiao() == "Nordeste") {
+				lista.add(estado.nomeEstado);
+
+			}
+		}
+		return lista;
+	}
+
 }
--- src/test/java/br/com/db1/EstadosDoBrasilTest.java
+++ src/test/java/br/com/db1/EstadosDoBrasilTest.java
@@ -1,47 +1,39 @@
 package br.com.db1;
 
 import org.junit.Test;
-
+import static org.junit.Assert.*;
 import br.com.db1.type.EstadosDoBrasil;
 
 public class EstadosDoBrasilTest {
 
 	@Test
-	public void estadosRegiaoNorte(){		
-		EstadosDoBrasil[] estados = EstadosDoBrasil.values();
-		
-		for (EstadosDoBrasil estado : estados){
-			if(estado.regiao() == "Sul"){
-				System.out.println(estado + " é da região sul");
-			}
-		}
-		System.out.println();
-		
-		for (EstadosDoBrasil estado : estados){
-			if(estado.regiao() == "Sudeste"){
-				System.out.println(estado + " é da região sudeste");
-			}
-		}
-		System.out.println();
-		
-		for (EstadosDoBrasil estado : estados){
-			if(estado.regiao() == "Centro Oeste"){
-				System.out.println(estado + " é da região Centro Oeste");
-			}
-		}
-		System.out.println();
-		
-		for (EstadosDoBrasil estado : estados){
-			if(estado.regiao() == "Norte"){
-				System.out.println(estado + " é da região Norte");
-			}
-		}
-		System.out.println();
-		
-		for (EstadosDoBrasil estado : estados){
-			if(estado.regiao() == "Nordeste"){
-				System.out.println(estado + " é da região Nordeste");
-			}
-		}
+	public void estadosRegiaoSulTest(){
+		assertTrue(EstadosDoBrasil.estadosRegiaoSul().contains(EstadosDoBrasil.PARANA.nomeEstado()));
+		assertTrue(EstadosDoBrasil.estadosRegiaoSul().contains(EstadosDoBrasil.SANTA_CATARINA.nomeEstado()));
+		assertFalse(EstadosDoBrasil.estadosRegiaoSul().contains(EstadosDoBrasil.SAO_PAULO.nomeEstado()));
+	}
+	@Test
+	public void estadosRegiaoSudesteTest(){
+		assertTrue(EstadosDoBrasil.estadosRegiaoSudeste().contains(EstadosDoBrasil.SAO_PAULO.nomeEstado()));
+		assertTrue(EstadosDoBrasil.estadosRegiaoSudeste().contains(EstadosDoBrasil.ESPIRITO_SANTO.nomeEstado()));
+		assertFalse(EstadosDoBrasil.estadosRegiaoSudeste().contains(EstadosDoBrasil.BAHIA.nomeEstado()));
+	}
+	@Test
+	public void estadosRegiaoCentroOesteTest(){
+		assertTrue(EstadosDoBrasil.estadosRegiaoCentroOeste().contains(EstadosDoBrasil.DISTRITO_FEDERAL.nomeEstado()));
+		assertTrue(EstadosDoBrasil.estadosRegiaoCentroOeste().contains(EstadosDoBrasil.MATO_GROSSO.nomeEstado()));
+		assertFalse(EstadosDoBrasil.estadosRegiaoCentroOeste().contains(EstadosDoBrasil.BAHIA.nomeEstado()));
+	}
+	@Test
+	public void estadosRegiaoNorteTest(){
+		assertTrue(EstadosDoBrasil.estadosRegiaoNorte().contains(EstadosDoBrasil.AMAPA.nomeEstado()));
+		assertTrue(EstadosDoBrasil.estadosRegiaoNorte().contains(EstadosDoBrasil.AMAZONAS.nomeEstado()));
+		assertFalse(EstadosDoBrasil.estadosRegiaoNorte().contains(EstadosDoBrasil.BAHIA.nomeEstado()));
+	}
+	@Test
+	public void estadosRegiaoNordesteTest(){
+		assertTrue(EstadosDoBrasil.estadosRegiaoNordeste().contains(EstadosDoBrasil.BAHIA.nomeEstado()));
+		assertTrue(EstadosDoBrasil.estadosRegiaoNordeste().contains(EstadosDoBrasil.MARANHAO.nomeEstado()));
+		assertFalse(EstadosDoBrasil.estadosRegiaoNordeste().contains(EstadosDoBrasil.SAO_PAULO.nomeEstado()));
 	}
 }
